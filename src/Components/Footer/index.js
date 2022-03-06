25e530caa035ffb6f2e1519fa01a635aecc6ce89
Malachi <malachialusa@gmail.com>
2022-03-06 16:10:43 -0700

    I added basic Schedule Button, Footer, Banner, and disclosure to App

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function Footer(){
+
+    return(
+
+
+            <div className='MobileCardTextFooter'>This is an application designed to help members 
+                who attend The Briderland Park 2nd Ward located in 
+                Logan Utah. This is designed to help members schedule Appointments  
+                <div className='PageBreak'></div>
+                This site is NOT maintained or produced by 
+                The Church of Jesus Christ of Latter day Saints
+                <div className='PageBreak'></div>
+                This is a Tool developed by Brother, Malachi Alusa in an effort
+                to better organize and help Church efforts 
+            </div>
+
+
+                
+
+    )
+};
+
+export default Footer
--- src/Components/Mobile/index.js
+++ src/Components/Mobile/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import {Breakpoint} from 'react-socks';
+import Footer from "../Footer";
 
 function Mobile() {
 
+    const [ScheduleButton, SetScheduleButton]=React.useState(false);
+
+
+    function ScheduleView (){
+
+        if(ScheduleButton === false){
+            return(
+                <div className='MobileScheduleButtonFlex'>
+                        <button onClick={ScheduleUIClick}className="MobileScheduleButton">Schedule Appointments</button> 
+                </div>
+            )
+        };
+
+        if(ScheduleButton === true){
+            return(
+                <div className='MobileBgFlex'>
+
+                    <div className="MobileCard">
+                        <div className='MobileCardTextTitle'> Schedule an Appointment
+                        </div>
+
+                        <div className='MobileCardText'>Please Select an available time
+                        </div>
+
+                    </div>
+                    
+                </div>
+            )
+        }
+    };
+
+    /*handles the Schedule Button Click*/ 
+    function ScheduleUIClick(){
+        SetScheduleButton(true);
+    }
 
 
     return(
@@ -16,11 +52,16 @@ function Mobile() {
             <div className='MobileBgFlex'>
 
                  <div className="MobileCard">
-                    <div>This is test text</div>    
+                    <div className='MobileCardText'> Relief Society Ministering Interviews
+                    </div>
                 </div>
-                
+                    
             </div>
 
+            <ScheduleView/>
+
+            
+            <Footer/>
                
 
         </div>
