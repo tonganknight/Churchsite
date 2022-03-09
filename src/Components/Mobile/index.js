ff8955a218e5a34734390097af7b701e086acf98
Malachi <malachialusa@gmail.com>
2022-03-09 09:00:54 -0700

    I pulled both month and date to the front end

--- src/Components/Mobile/index.js
+++ src/Components/Mobile/index.js
@@ -1,9 +1,11 @@
-import React, { Children, useState } from 'react';
+import React, { useState } from 'react';
 import {Breakpoint} from 'react-socks';
 import Footer from "../Footer";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import DatePicker from 'sassy-datepicker';
+
+var moment = require('moment');
 
 function Mobile() {
 
@@ -19,19 +21,40 @@ function Mobile() {
 
     const onChange = (date) => {
         let DatePulled = date.toString();
-        console.log(DatePulled);
-        let NewArrDate = [...SelectedDate];
-        
+
+
+        /*for some reason saving this to state freezes datepicker*/
+
+        /*let dateObject = new Date(DatePulled);
+        let momentObject = moment(dateObject);
+        let momentString = momentObject.format('MMMM Do YYYY');
+        let NewArrDate =[...SelectedDate];
+        NewArrDate.splice(0, 1, {STime: `${momentString}`});
+
+        SetSelectedDate(NewArrDate);*/
 
     }
 
     function SubmitClick(){
         /*Select the Dropdown Button Class because the created did not pass down a prop for it*/
         let Dbox = document.getElementsByClassName("is-selected");
+        if(Dbox == "undefined"){
+            window.alert("You must select a date")
+        };
         /*Target the returned Elements, there is only one with is-selected class*/
-        let Dselect =Dbox[0].innerHTML
+        let Dselect =Dbox[0].innerHTML;
         /* Target the inner html at place 0 so we can see what is selected on the dropdown button*/
-        console.log(Dselect);
+        /*get the Month on the datepicker*/
+        let MTitle = document.getElementsByClassName("sdp--text sdp--month-name");
+        let Month =MTitle[0].innerHTML;
+        /*get the Date on the datepicker*/
+        let DDate = document.getElementsByClassName("sdp--date-btn__selected");
+        let Date = DDate[0].innerHTML;
+        console.log(Month + " " + Date);
+
+
+
+  
 
 
     }
